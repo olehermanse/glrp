5703d17d8b1a10d1e6cc1c78397b488ee40d19d5
Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>
2025-06-03 03:09:51 +0200

    Fixed license in setup.py
    Signed-off-by: Ole Herman Schumacher Elgesem <ole.elgesem@northern.tech>

--- setup.py
+++ setup.py
@@ -29,8 +29,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name="glrp",
     version=glrp_version,
-    author="Northern.tech, Inc.",
-    author_email="contact@northern.tech",
+    author="Ole Herman Schumacher Elgesem",
+    author_email="",
     description="Parser for git log --raw",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -40,7 +40,7 @@ setuptools.setup(
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
